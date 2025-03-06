d05372abb5bd8789af131fb3b2806d9b9a17ccac
Pedzisai Poseni <pedzisaiposeni@gmail.com>
2025-03-06 14:26:29 +0200

    Learning full python

---
+++ Moshproject.py
@@ -0,0 +1,22 @@
+# Full_name = "Jonh Smith"
+# age = 20
+# Is_new = True
+# print(Full_name)
+# print("Age", age)
+# print("Is_new:", Is_new)
+
+#Ask tow questions persons name and color and the print "Charmu likes Black"
+
+# name =input("What is your name: ")
+# color = input("What is your favourite color: ")
+# print( name + " likes " + color)
+
+#Write a program that will require birth year and quickly calculates your age and the print your age on the terminal
+
+Birth_year = input("Enter Birth Year: ")
+age = 2025 - int(Birth_year)
+print( "Age: ", age)
+
+
+
+
