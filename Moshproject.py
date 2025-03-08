d242feef153e5e797219db832d47ac4df0d091e9
Pedzisai Poseni <pedzisaiposeni@gmail.com>
2025-03-08 23:15:25 +0200

    continued saturday coding

--- Moshproject.py
+++ Moshproject.py
@@ -12,10 +12,73 @@
 # print( name + " likes " + color)
 
 #Write a program that will require birth year and quickly calculates your age and the print your age on the terminal
+#
+# Birth_year = input("Enter Birth Year: ")
+# age = 2025 - int(Birth_year)
+# print( "Age: ", age)
 
-Birth_year = input("Enter Birth Year: ")
-age = 2025 - int(Birth_year)
-print( "Age: ", age)
+# #Ask the user their weighty  in pounds lbs and then convert it to kilograms kgs and print it on the terminal
+# weight_pounds = input( "What is your weighty in pounds?" )
+# your_kgs =int(weight_pounds) * 0.45
+# print( "Weight:", your_kgs, "kgs" )
+
+# import math
+# print(math.ceil(5.6))
+
+# Create a program that runs the following
+#If its a hot day
+    #its a hot day
+    #Drinks lots of water
+#Otherwise
+    #Its a cold day
+    #Wear warm clothes
+#Otherwise
+    #its a lovely day
+
+
+# is_hot = False
+# is_cold = True
+#
+# if is_hot:
+#      print("Its a hot day")
+#      print("Drink lots of water")
+# elif is_cold:
+#     print("Its a cold day")
+#     print("Wear warm clothes")
+# else:
+#      print("Its a lovely day")
+
+# input temperature for the conditions to work
+
+day_temp = float(input("What is the temperature today: "))
+
+# Define the conditions
+is_hot = (day_temp > 20)
+is_cold = (day_temp < 15)
+if is_hot:
+     print("Its a hot day")
+     print("Drink lots of water")
+elif is_cold:
+    print("Its a cold day")
+    print("Wear warm clothes")
+else:
+     print("Its a lovely day")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
